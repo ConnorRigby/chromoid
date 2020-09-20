17ea6ae12f5540c1a85d05b7baaf819c6e02a49f
Connor Rigby <konnorrigby@gmail.com>
2020-09-20 13:02:12 -0700

    android stuff?

--- chromoid_link_android/app/src/main/java/id/chromo/link/MainActivity.kt
+++ chromoid_link_android/app/src/main/java/id/chromo/link/MainActivity.kt
@@ -3,6 +3,39 @@ package id.chromo.link
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import ch.kuon.phoenix.Socket
+import ch.kuon.phoenix.Channel
+import ch.kuon.phoenix.Presence
+
+
+fun doSomething() {
+    val url = "ws://localhost:4000/device_socket/websocket?token=gQwn5GgQ8rExwlb_naec_60Tc_FVWROj5lrBxs9Cm60"
+    val sd = Socket(url)
+
+    sd.connect()
+
+    val chan = sd.channel("devices")
+
+    chan
+    .join()
+    .receive("ok") { msg ->
+        // channel is connected
+    }
+    .receive("error") { msg ->
+        // channel did not connected
+    }
+    .receive("timeout") { msg ->
+        // connection timeout
+    }
+
+    chan
+    .push("hello")
+    .receive("ok") { msg ->
+        // sent hello and got msg back
+    }
+
+}
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
