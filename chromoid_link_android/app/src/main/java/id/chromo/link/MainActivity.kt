e9edf1791b1efcc3011fb1c10f101a2239247b3e
Connor Rigby <connorr@hey.com>
2020-09-20 16:07:39 -0700

    Implement channel connection

--- chromoid_link_android/app/src/main/java/id/chromo/link/MainActivity.kt
+++ chromoid_link_android/app/src/main/java/id/chromo/link/MainActivity.kt
@@ -1,44 +1,66 @@
 package id.chromo.link
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import ch.kuon.phoenix.Socket
-import ch.kuon.phoenix.Channel
-import ch.kuon.phoenix.Presence
+
+fun socketInit(mainActivity: MainActivity) {
 
 
-fun doSomething() {
-    val url = "ws://localhost:4000/device_socket/websocket?token=gQwn5GgQ8rExwlb_naec_60Tc_FVWROj5lrBxs9Cm60"
-    val sd = Socket(url)
+    val opts = Socket.Options()
+    opts.timeout = 5_000
+    opts.heartbeatIntervalMs = 10_000
+    opts.rejoinAfterMs = { tries -> tries * 500}
+    opts.reconnectAfterMs = { tries -> tries * 500}
+    opts.params = HashMap<String, Any> ()
 
-    sd.connect()
+    val url = "ws://10.0.2.2:4000/device_socket" // dev
+    opts.params?.set("token", "m_CVZWnscA-eyajZzx180YBQNnII2bXa4hv1JrLqwRw") // dev
 
-    val chan = sd.channel("devices")
+    //val url = "wss://chromo.id/device_socket" // prod
+    //opts.params?.set("token", "DGR_xUctGXrVHhxrVDaPHQu5rMYOxuN1lAJCUkr1U4k") // prod
+
+
+    val socket = Socket(url, opts)
+    val button = mainActivity.findViewById<Button>(R.id.connectButton)
+    button.text = "Initializing...";
+
+    socket.onError {
+        println("There was an error with the connection!")
+    }
+    socket.onClose { i: Int, s: String -> println("The connection closed!") }
+    socket.connect()
+
+    val chan = socket.channel("device")
+    var changeText = { value: String, color: Int ->
+        button.text = value
+        button.setBackgroundColor(color)
+    }
 
     chan
     .join()
     .receive("ok") { msg ->
-        // channel is connected
+        println("Connected to device channel " + mainActivity.getString(R.string.connectionOK))
+        changeText(mainActivity.getString(R.string.connectionOK), Color.GREEN)
     }
     .receive("error") { msg ->
-        // channel did not connected
+        changeText(R.string.connectionErr.toString(), Color.RED)
     }
     .receive("timeout") { msg ->
-        // connection timeout
+        changeText(R.string.connectionErrTimeout.toString(), Color.YELLOW)
     }
-
-    chan
-    .push("hello")
-    .receive("ok") { msg ->
-        // sent hello and got msg back
-    }
-
 }
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.connectButton)
+        button?.setOnClickListener()
+        {
+            socketInit(this)
+        }
     }
 }
